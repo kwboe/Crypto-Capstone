20896d1b45ec304690a7e0af4d57f7018085d744
Torin Kearney <torin.h.kearney@uscga.edu>
2025-04-10 05:46:31 -0400

    add client sync

---
+++ client_sync.c
@@ -0,0 +1,10 @@
+#include "net_sync.h"
+
+/*
+ * This file can contain additional client-specific helper functions.
+ * Currently, all the network synchronization functions are defined in net_sync.c.
+ *
+ * For example, if you later want to add client-only routines,
+ * you can do so here.
+ */
+
